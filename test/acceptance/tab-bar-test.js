c3aa7c6b93f96127952eda301d730c0eb526e602
OsamaSayegh <asooomaasoooma90@gmail.com>
2021-04-20 06:26:59 +0300

    Add a couple of tests

---
+++ test/acceptance/tab-bar-test.js
@@ -0,0 +1,22 @@
+import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
+import { visit } from "@ember/test-helpers";
+import { test } from "qunit";
+
+acceptance("Tab bar component | Logged in users on mobile", function(needs) {
+  needs.user();
+  needs.mobileView();
+
+  test("They see the bar on mobile", async function(assert) {
+    await visit("/latest");
+    assert.ok(exists(".d-tab-bar-theme"));
+  });
+});
+
+acceptance("Tab bar component | Anon users on mobile", function(needs) {
+  needs.mobileView();
+
+  test("They don't see the bar on mobile", async function(assert) {
+    await visit("/latest");
+    assert.ok(!exists(".d-tab-bar-theme"));
+  });
+});
